7b8587e06f4ba0c1c178fbe0c8bacf8f012b5e1b
Simone Lusenti <simone@plasticpanda.com>
2017-02-13 17:56:36 +0100

    Update index.js

--- dynamodb-backup/index.js
+++ dynamodb-backup/index.js
@@ -50,7 +50,9 @@ const lambdaRoleTemplate = (tableLogicalName, bucketLogicalName) => ({
   }
 });
 
-module.exports = function createDynamodbBackupTrigger (tableLogicalName, bucketLogicalName) {
+module.exports = function createDynamodbBackupTrigger (params) {
+  const tableLogicalName = params.tableLogicalName;
+  const bucketLogicalName = params.bucketLogicalName;
   const streamArn = { 'Fn::GetAtt': [tableLogicalName, 'StreamArn'] };
   const lambdaName = lambdaNameTemplate(tableLogicalName);
   return {
