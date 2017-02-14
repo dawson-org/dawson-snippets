b7e7b91f23f1bec71272d756cf6ea4c92c3d160a
Simone Lusenti <simone@plasticpanda.com>
2017-02-14 14:39:48 +0100

    Create index.js

---
+++ s3-bucket-public-read/index.js
@@ -0,0 +1,34 @@
+const makePolicyLogicalName = bucketLogicalName =>
+  `${bucketLogicalName}Policy`;
+
+module.exports = params => {
+  const bucketLogicalName = params.bucketLogicalName;
+  return {
+    [bucketLogicalName]: {
+      Type: 'AWS::S3::Bucket',
+      Properties: {},
+      DeletionPolicy: 'Retain'
+    },
+    [makePolicyLogicalName]: {
+      Type: 'AWS::S3::BucketPolicy',
+      Properties: {
+        Bucket: { Ref: bucketLogicalName },
+        PolicyDocument: {
+          Statement: [
+            {
+              Action: ['s3:GetObject'],
+              Effect: 'Allow',
+              Resource: {
+                'Fn::Join': [
+                  '',
+                  ['arn:aws:s3:::', { Ref: bucketLogicalName }, '/*']
+                ]
+              },
+              Principal: '*'
+            }
+          ]
+        }
+      }
+    }
+  };
+};
